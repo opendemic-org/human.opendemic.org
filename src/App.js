caab301921e5b0063296330398297fd10dbad218
Val <valmack@protonmail.com>
2020-03-26 22:29:58 -0400

    Add blueprint header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./pages/Home";
+
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
-          <div>Opendemic</div>
           <Route path="/">
             <Home />
           </Route>
