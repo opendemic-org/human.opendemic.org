0b6700766df6aac739cb527179c587c769009216
Val <valmack@protonmail.com>
2020-04-17 20:37:10 -0400

    Add sentry env var

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const IP_STACK_ACCESS_KEY = process.env.REACT_APP_IP_STACK_ACCESS_KEY;
 export const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+export const SENTRY_DSN = process.env.REACT_APP_SENTRY_DSN;
