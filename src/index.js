9697a6492279788efb72156fa590971dfa7aba7a
Val <valmack@protonmail.com>
2020-05-04 17:49:49 -0400

    Add sentry config

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import * as Sentry from "@sentry/browser";
 
 import App from "./App";
 
+import { SENTRY_DSN } from "./config";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./store/configureStore";
 import "./index.css";
 
 const { store, persistor } = configureStore();
+
+Sentry.init({ dsn: SENTRY_DSN });
 
 ReactDOM.render(
   <React.StrictMode>
